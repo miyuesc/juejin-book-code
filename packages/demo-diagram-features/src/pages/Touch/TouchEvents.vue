fb714085c81de7c37ba779857e4b3b8536d84cd8
MiyueSC <913784771@qq.com>
2023-10-16 16:25:34 +0800

    1

--- packages/demo-diagram-features/src/pages/Touch/TouchEvents.vue
+++ packages/demo-diagram-features/src/pages/Touch/TouchEvents.vue
@@ -3,17 +3,27 @@
   import Diagram from 'diagram-js'
   import TouchModule from 'diagram-js/lib/features/touch'
   import { bootstrapShapes } from '../../utils/bootstrap.js'
+  import InteractionEventsModule from 'diagram-js/lib/features/interaction-events'
 
   const bootstrapDiagram = (id) => {
     return new Diagram({
-      canvas: { container: document.getElementById(id) }
-      // modules: [TouchModule]
+      canvas: { container: document.getElementById(id) },
+      modules: [
+        InteractionEventsModule
+        // TouchModule
+      ]
     })
   }
+
+  const events = ['element.click', 'element.contextmenu', 'element.dblclick', 'element.mousedown', 'element.mousemove', 'element.hover', 'element.out', 'element.mouseup']
 
   onMounted(() => {
     const djs = bootstrapDiagram('touch-canvas')
     bootstrapShapes(djs.get('canvas'))
+
+    djs.get('eventBus').on(events, ({ element, gfx, originalEvent, type }) => {
+      console.log('eventType: ', type, ', element: ', element.id)
+    })
   })
 </script>
 
