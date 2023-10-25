87170c6fe18faca34250108c0b0e0c222e7f36fb
MiyueSC <913784771@qq.com>
2023-10-25 13:41:21 +0800

    1

--- packages/demo-diagram-features/src/pages/DraggingAndMove/DraggingAndMoveModule.vue
+++ packages/demo-diagram-features/src/pages/DraggingAndMove/DraggingAndMoveModule.vue
@@ -5,12 +5,17 @@
   import ModelingModule from 'diagram-js/lib/features/modeling'
   import { bootstrapShapes } from '../../utils/bootstrap.js'
 
-  import CustomRules from '../../modules/customRules.js'
+  // import CustomRules from '../../modules/customRules.js'
 
   const bootstrapDiagram = (id) => {
     return new Diagram({
       canvas: { container: document.getElementById(id) },
-      modules: [MoveModule, ModelingModule, CustomRules]
+      modules: [
+        MoveModule,
+        ModelingModule
+        // 自定义规则
+        // CustomRules
+      ]
     })
   }
 
@@ -18,12 +23,19 @@
     const djs = bootstrapDiagram('dragging-canvas')
     bootstrapShapes(djs.get('canvas'))
 
-    // djs.get('eventBus').on('shape.move.start', 1300, ({ context }) => {
-    //   const { validatedShapes, target } = context
-    //   if (validatedShapes.findIndex((i) => i.id === 's1') > -1) {
-    //     return false
-    //   }
-    // })
+    djs.get('eventBus').on('shape.move.start', 1300, ({ context }) => {
+      const { validatedShapes } = context
+      if (validatedShapes.findIndex((i) => i.id === 's1') > -1) {
+        return false
+      }
+    })
+
+    djs.get('eventBus').on('shape.move.move', 300, ({ context }) => {
+      const { target } = context
+      if (target && target.id === 's3') {
+        context.canExecute = false
+      }
+    })
 
     djs.get('eventBus').on('shape.move.end', () => {
       console.log(djs.get('elementRegistry').getAll())
