0342dcba17d03862359f626ab7bc2a38e5ff35d4
MiyueSC <913784771@qq.com>
2023-11-13 16:49:03 +0800

    Update ModdelDemo.vue

--- packages/demo-bpmn-moddle/src/pages/main/ModdelDemo.vue
+++ packages/demo-bpmn-moddle/src/pages/main/ModdelDemo.vue
@@ -4,21 +4,21 @@
 
   const model = new Moddle([Properties])
 
-  const ComplexType = model.getType('props:Complex')
-  const SimpleBody = model.getType('props:SimpleBody')
-  const Attributes = model.getType('props:Attributes')
+  console.log(model)
 
+  // const ComplexType = model.getType('props:Complex')
+  // const SimpleBody = model.getType('props:SimpleBody')
+  // const Attributes = model.getType('props:Attributes')
+  // console.log(ComplexType, SimpleBody, Attributes)
+  //
   const complexTypeInst = model.create('props:Complex', { name: 'complex', id: 'complex', idx: 1 })
   const simpleBodyInst = model.create('props:SimpleBody', { body: 'simple', idx: 2 })
   const attributesInst = model.create('props:Attributes', { attrs: [complexTypeInst, simpleBodyInst], idx: 3 })
-
-  const foo = model.createAny('vendor:Foo', 'http://vendor', { value: 'bar' })
-  const container = model.createAny('my:Container', 'http://my', { any: [foo] })
-
-  console.log(model)
-  console.log(ComplexType, SimpleBody, Attributes)
   console.log(complexTypeInst, simpleBodyInst, attributesInst)
-  console.log(foo, container)
+  //
+  // const foo = model.createAny('vendor:Foo', 'http://vendor', { value: 'bar' })
+  // const container = model.createAny('my:Container', 'http://my', { any: [foo] })
+  // console.log(foo, container)
 </script>
 
 <template>
