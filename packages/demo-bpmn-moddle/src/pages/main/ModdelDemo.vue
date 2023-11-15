4e1ff0db9d1e734c9385bfd869845a2568965c6c
MiyueSC <913784771@qq.com>
2023-11-15 16:23:53 +0800

    1

--- packages/demo-bpmn-moddle/src/pages/main/ModdelDemo.vue
+++ packages/demo-bpmn-moddle/src/pages/main/ModdelDemo.vue
@@ -1,21 +1,36 @@
 <script setup>
   import { Moddle } from 'moddle'
   import Properties from '../../model/properties.json'
+  import Base from '../../model/extension/base.json'
+  import Custom from '../../model/extension/custom.json'
 
   const model = new Moddle([Properties])
 
   console.log(model)
 
+  // getType
   const ComplexType = model.getType('props:Complex')
   const SimpleBody = model.getType('props:SimpleBody')
   const Attributes = model.getType('props:Attributes')
   console.log(ComplexType, SimpleBody, Attributes)
 
+  // initiate
   const complexTypeInst = model.create('props:Complex', { name: 'complex', id: 'complex', idx: 1 })
   const simpleBodyInst = model.create('props:SimpleBody', { body: 'simple', idx: 2 })
   const attributesInst = model.create('props:Attributes', { attrs: [complexTypeInst, simpleBodyInst], idx: 3 })
-  console.log(complexTypeInst, simpleBodyInst, attributesInst)
 
+  // serialize
+  const instance1 = model.create('props:Attributes')
+  instance1.set('props:booleanValue', true)
+  instance1.set('props:integerValue', -1000)
+  const instance2 = model.create('props:Attributes')
+  instance1.set('props:booleanValue', false)
+  instance1.set('props:realValue', -0.02)
+  const complexAttrs = model.create('props:ComplexAttrs', { attrs: instance1 })
+  const complexAttrsCol = model.create('props:ComplexAttrsCol', { attrs: [instance1, instance2] })
+  console.log('serialize', complexAttrs, complexAttrsCol)
+
+  // getProperty
   const properties = model.properties
   console.log(properties.getProperty(complexTypeInst, 'name'))
   console.log(properties.getProperty(simpleBodyInst, 'body'))
@@ -25,8 +40,19 @@
   // const foo = model.createAny('vendor:Foo', 'http://vendor', { value: 'bar' })
   // const container = model.createAny('my:Container', 'http://my', { any: [foo] })
   // console.log(foo, container)
+
+  const model2 = new Moddle([Base, Custom])
+
+  console.log(model2)
+
+  const Root = model2.getType('b:Root')
+  const CustomRoot = model2.getType('c:CustomRoot')
+  console.log(new Root())
+  console.log(new CustomRoot())
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <p>打开控制台查看输出</p>
+  </div>
 </template>
