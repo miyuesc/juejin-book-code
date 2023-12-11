8ae6b2fb610563efe66f6297a1c58627d905d290
MiyueSC <913784771@qq.com>
2023-12-11 17:03:19 +0800

    moddle-xml

--- packages/demo-bpmn-moddle/src/pages/main/ModdleXmlWriterDemo.vue
+++ packages/demo-bpmn-moddle/src/pages/main/ModdleXmlWriterDemo.vue
@@ -24,26 +24,43 @@
     refs: [complex1, complex2, complex3]
   })
 
+  const complexCount = model.create('props:ComplexCount', { id: 'ComplexCount_1' })
+  const complexNesting = model.create('props:ComplexNesting', { id: 'ComplexNesting_1' })
+
+  const referencingCollection = model.create('props:ReferencingCollection', {
+    references: [complexCount, complexNesting]
+  })
+  complexCount.$parent = complexNesting.$parent = referencingCollection
+
   const simpleBody1 = model.create('props:SimpleBody', { body: 'foo', otherAttr: 'other foo' })
   const simpleBody2 = model.create('props:SimpleBody', { body: 'bar', anotherAttr: 'another bar' })
   const simpleBody3 = model.create('props:SimpleBody', { body: 'baz', anotherAttr: 'another baz' })
 
-  const colAttr1 = model.create('props:Attributes', { name: 'integerValue', value: 10 })
-  const colAttr2 = model.create('props:Attributes', { name: 'booleanValue', value: true })
+  const colAttr1 = model.create('props:Attributes', { name: 'integerValue', value: 10, arr: [1, 2, 3] })
+  const colAttr2 = model.create('props:Attributes', { name: 'booleanValue', value: true, obj: { a: 1, b: 2, c: 3 } })
   const complexAttrsCol = model.create('props:ComplexAttrsCol', {
     attrs: [colAttr1, colAttr2]
   })
   colAttr1.$parent = colAttr2.$parent = complexAttrsCol
 
-  const attr = model.create('props:Attributes', { name: 'booleanValue', value: true })
+  const colAttr3 = model.create('props:Attributes', { name: 'integerValue', value: 10 })
+  const complexAttrs = model.create('props:ComplexAttrs', {
+    attrs: colAttr3
+  })
+  colAttr3.$parent = complexAttrs
+
+  const propertyValue = model.create('props:BaseWithId', { id: 'PropertyValue' })
+  const typeValue = model.create('props:BaseWithId', { id: 'PropertyValue' })
+  const defineValue = model.create('props:BaseWithId', { id: 'PropertyValue' })
+  const withProperty = model.create('props:WithProperty', { propertyName: propertyValue, typeName: typeValue, defineName: defineValue })
 
   const root = model.create('props:Root', {
-    any: [attr, simpleBody1, simpleBody2, simpleBody3, containedCollection, attributeReferenceCollection, complexAttrsCol],
+    any: [simpleBody1, simpleBody2, simpleBody3, containedCollection, attributeReferenceCollection, referencingCollection, complexAttrs, complexAttrsCol, withProperty],
     otherAttr: 'other root',
     anotherAttr: 'another root'
   })
-  attr.$parent = simpleBody1.$parent = simpleBody2.$parent = simpleBody3.$parent = root
-  containedCollection.$parent = attributeReferenceCollection.$parent = complexAttrsCol.$parent = root
+  simpleBody1.$parent = simpleBody2.$parent = simpleBody3.$parent = withProperty.$parent = root
+  containedCollection.$parent = attributeReferenceCollection.$parent = referencingCollection.$parent = complexAttrsCol.$parent = root
 
   console.log(root)
 
